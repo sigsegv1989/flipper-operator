071544051123875b2cbc29720fc62edb75d1a5ec
sigsegv1989 <sigsegv1989@gmail.com>
2024-06-17 23:53:37 +0530

    Added reconcile logic for rolling restart of deployments with annotations for tracing
    - Implemented reconcile logic in `RollingUpdateReconciler` to perform rolling restarts of deployments based on specified interval and matchLabels in the RollingUpdate CR.
    - Added the following annotations to deployments during the restart process to facilitate tracing:
      - `"kubectl.kubernetes.io/restartedAt"`: Timestamp of the restart
      - `"kubectl.kubernetes.io/restartedBy"`: Indicating the restart was triggered by the flipper-operator
      - `"flipper.example.com/restartedByCR"`: Namespaced name of the RollingUpdate CR that triggered the restart
      - `"flipper.example.com/restartedByCRDKind"`: Kind of the CRD (RollingUpdate)
    
    - Created a test sample deployment `nginx-deployment` with the label `app: nginx`.
    - Added a sample `RollingUpdate` CR to match deployments with the `app: nginx` label and to initiate rolling restarts at a specified interval of `3m, 5m and 10m`.

--- go.mod
+++ go.mod
@@ -63,6 +63,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.10 // indirect
 	k8s.io/api v0.30.0 // indirect
 	k8s.io/apiextensions-apiserver v0.30.0 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
--- internal/controller/rollingupdate_controller.go
+++ internal/controller/rollingupdate_controller.go
@@ -18,18 +18,25 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	flipperv1alpha1 "github.com/sigsegv1989/flipper-operator/api/v1alpha1"
 )
 
 // RollingUpdateReconciler reconciles a RollingUpdate object
 type RollingUpdateReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
 
@@ -47,15 +54,115 @@ type RollingUpdateReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.2/pkg/reconcile
 func (r *RollingUpdateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := r.Log.WithValues("rollingupdate", req.NamespacedName)
 
-	// TODO(user): your logic here
+	// Fetch the RollingUpdate CR instance
+	rollingUpdate := &flipperv1alpha1.RollingUpdate{}
+	err := r.Get(ctx, req.NamespacedName, rollingUpdate)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("RollingUpdate resource not found. Ignoring reconcile...")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to fetch RollingUpdate")
+		return ctrl.Result{}, err
+	}
+	log.V(1).Info("Successfully retrieved RollingUpdate resource", "rollingUpdate", rollingUpdate)
 
-	return ctrl.Result{}, nil
+	interval, err := time.ParseDuration(rollingUpdate.Spec.Interval)
+	if err != nil {
+		log.Error(err, "Failed to parse interval duration", "interval", rollingUpdate.Spec.Interval)
+		return ctrl.Result{}, err
+	}
+	log.V(1).Info("Successfully retrieved RollingUpdate interval", "interval", interval)
+
+	now := time.Now()
+	if rollingUpdate.Status.LastRolloutTime.Time.IsZero() ||
+		now.Sub(rollingUpdate.Status.LastRolloutTime.Time) > interval {
+
+		log.V(1).Info("Time to rolling restart resources", "lastRolloutTime", rollingUpdate.Status.LastRolloutTime, "now", now, "interval", interval)
+
+		err = r.restartDeployments(ctx, req, rollingUpdate.Spec.MatchLabels)
+		if err != nil {
+			log.Error(err, "Failed to restart deployments")
+			return ctrl.Result{}, err
+		}
+
+		rollingUpdate.Status.LastRolloutTime = metav1.Now()
+		err = r.Status().Update(ctx, rollingUpdate)
+		if err != nil {
+			log.Error(err, "Failed to update rollingUpdate status")
+			return ctrl.Result{}, err
+		}
+
+		log.Info("Successfully rolling restarted resource and updated RollingUpdate status", "lastRolloutTime", rollingUpdate.Status.LastRolloutTime)
+	}
+
+	return ctrl.Result{RequeueAfter: interval}, nil
+}
+
+func (r *RollingUpdateReconciler) restartDeployments(ctx context.Context, req ctrl.Request, labels map[string]string) error {
+	log := r.Log.WithValues("namespace", req.Namespace, "name", req.Name)
+
+	log.V(1).Info("Listing deployments for rolling restart", "labels", labels)
+	log.V(1).Info("Checking deployment labels", "namespace", req.Namespace, "expectedLabels", labels)
+
+	deployments := &appsv1.DeploymentList{}
+	// labels = map[string]string{"app": "nginx"}
+	err := r.List(ctx, deployments, client.InNamespace(req.Namespace), client.MatchingLabels(labels))
+	if err != nil {
+		log.Error(err, "Failed to list deployments", "labels", labels)
+		return err
+	}
+	log.V(1).Info("Deployments listed", "deploymentCount", len(deployments.Items))
+
+	for _, deployment := range deployments.Items {
+		log.V(1).Info("Restarting deployment", "namespace", deployment.Namespace, "name", deployment.Name)
+
+		annotations := map[string]string{
+			"kubectl.kubernetes.io/restartedAt":      time.Now().Format(time.RFC3339),
+			"kubectl.kubernetes.io/restartedBy":      "flipper-operator",
+			"flipper.example.com/restartedByCR":      fmt.Sprintf("%s/%s", req.Namespace, req.Name),
+			"flipper.example.com/restartedByCRDKind": "rollingupdate",
+		}
+		r.updateAnnotations(&deployment, annotations)
+
+		err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			return r.Update(ctx, &deployment)
+		})
+
+		if err != nil {
+			log.Error(err, "Failed to update deployment", "name", deployment.Name)
+			return fmt.Errorf("failed to update Deployment %s/%s: %v", deployment.Namespace, deployment.Name, err)
+		}
+
+		log.Info("Successfully rolling restarted deployment", "name", deployment.Name)
+	}
+	return nil
+}
+
+func (r *RollingUpdateReconciler) updateAnnotations(deployment *appsv1.Deployment, annotations map[string]string) {
+	// Update the Deployment's annotations
+	if deployment.Annotations == nil {
+		deployment.Annotations = make(map[string]string)
+	}
+	for key, value := range annotations {
+		deployment.Annotations[key] = value
+	}
+
+	// Update the pod template spec's annotations to trigger a rollout
+	if deployment.Spec.Template.Annotations == nil {
+		deployment.Spec.Template.Annotations = make(map[string]string)
+	}
+	for key, value := range annotations {
+		deployment.Spec.Template.Annotations[key] = value
+	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *RollingUpdateReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	r.Log = mgr.GetLogger().WithName("controller").WithName("RollingUpdate")
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&flipperv1alpha1.RollingUpdate{}).
 		Complete(r)
