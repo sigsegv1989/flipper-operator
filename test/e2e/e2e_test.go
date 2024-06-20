0c52257894ab05a69a522d9e71e5214143de1749
sigsegv1989 <sigsegv1989@gmail.com>
2024-06-20 22:22:13 +0530

    Added e2e tests for flipper operator functionality
    - Added e2e test to verify rolling restart of all deployments within a namespace without matchLabels.
    - Added e2e test to verify rolling restart of deployments with matchLabels within a namespace.
    - Added e2e test to verify no deployment is restarted when matchLabels do not match any depl	oyments within the namespace.

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -17,41 +17,102 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"os/exec"
+	"sort"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/sigsegv1989/flipper-operator/test/utils"
 )
 
 const namespace = "flipper-operator-system"
 
-var _ = Describe("controller", Ordered, func() {
-	BeforeAll(func() {
-		By("installing prometheus operator")
-		Expect(utils.InstallPrometheusOperator()).To(Succeed())
+var kindClusterName = "e2e-test"
+var KindClusterConfig = "/home/abhijeet/.kube/" + kindClusterName + ".kubeconfig"
 
-		By("installing the cert-manager")
-		Expect(utils.InstallCertManager()).To(Succeed())
+var _ = Describe("controller", Ordered, func() {
+
+	var clientset *kubernetes.Clientset
+	var dynamicClient dynamic.Interface
+
+	BeforeAll(func() {
+		By("exporting KIND_CLUSTER environment variable")
+		os.Setenv("KIND_CLUSTER", kindClusterName)
+
+		By("checking if the Kind cluster for e2e tests exists")
+		cmd := exec.Command("kind", "get", "clusters")
+		output, err := utils.Run(cmd)
+		Expect(err).NotTo(HaveOccurred())
+
+		if !strings.Contains(string(output), kindClusterName) {
+			By("creating a Kind cluster for e2e tests")
+			cmd = exec.Command("kind", "create", "cluster", "--name", kindClusterName, "--kubeconfig", KindClusterConfig)
+			_, err := utils.Run(cmd)
+			Expect(err).NotTo(HaveOccurred())
+		} else {
+			By("kind cluster already exists, skipping creation")
+		}
+
+		By("exporting KUBECONFIG environment variable")
+		os.Setenv("KUBECONFIG", KindClusterConfig)
+
+		By("load kubeconfig")
+		configPath := os.ExpandEnv(KindClusterConfig)
+		config, err := clientcmd.BuildConfigFromFlags("", configPath)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("create Kubernetes client")
+		clientset, err = kubernetes.NewForConfig(config)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("create dynamic client")
+		dynamicClient, err = dynamic.NewForConfig(config)
+		Expect(err).NotTo(HaveOccurred())
+
+		/*
+			By("installing prometheus operator")
+			Expect(utils.InstallPrometheusOperator()).To(Succeed())
+
+			By("installing the cert-manager")
+			Expect(utils.InstallCertManager()).To(Succeed())
+		*/
 
 		By("creating manager namespace")
-		cmd := exec.Command("kubectl", "create", "ns", namespace)
+		cmd = exec.Command("kubectl", "create", "ns", namespace)
 		_, _ = utils.Run(cmd)
 	})
 
 	AfterAll(func() {
-		By("uninstalling the Prometheus manager bundle")
-		utils.UninstallPrometheusOperator()
+		/*
+			By("uninstalling the Prometheus manager bundle")
+			utils.UninstallPrometheusOperator()
 
-		By("uninstalling the cert-manager bundle")
-		utils.UninstallCertManager()
+			By("uninstalling the cert-manager bundle")
+			utils.UninstallCertManager()
+		*/
 
 		By("removing manager namespace")
 		cmd := exec.Command("kubectl", "delete", "ns", namespace)
 		_, _ = utils.Run(cmd)
+
+		By("deleting the Kind cluster")
+		cmd = exec.Command("kind", "delete", "cluster", "--name", kindClusterName)
+		_, err := utils.Run(cmd)
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	Context("Operator", func() {
@@ -117,6 +178,256 @@ var _ = Describe("controller", Ordered, func() {
 			}
 			EventuallyWithOffset(1, verifyControllerUp, time.Minute, time.Second).Should(Succeed())
 
+		})
+	})
+
+	// Helper function to apply YAML manifest
+	applyManifest := func(path string) {
+		cmd := exec.Command("kubectl", "apply", "-f", path)
+		cmd.Env = append(cmd.Env, fmt.Sprintf("KUBECONFIG=%s", KindClusterConfig))
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Failed to apply manifest %s, error: %v, output: %s\n", path, err, string(output))
+			Expect(err).NotTo(HaveOccurred())
+		}
+	}
+
+	// Helper function to check if deployment pods have restarted
+	checkDeploymentPodsRestarted := func(deploymentName, namespace string) {
+		deployment, err := clientset.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Eventually(func() bool {
+			// Check if the annotation exists and is not empty
+			restartedAtExists := deployment.Annotations["kubectl.kubernetes.io/restartedAt"] != ""
+			restartedBy := deployment.Annotations["kubectl.kubernetes.io/restartedBy"] == "flipper-operator"
+			restartedByCRExists := deployment.Annotations["flipper.example.com/restartedByCR"] != ""
+			restartedByCRDKind := deployment.Annotations["flipper.example.com/restartedByCRDKind"] == "rollingupdate"
+
+			return restartedAtExists && restartedBy && restartedByCRExists && restartedByCRDKind
+		}, 2*time.Minute, 5*time.Second).Should(BeTrue())
+
+		// Construct a label selector that matches all labels in the deployment's pod template
+		selector := labels.Set(deployment.Spec.Selector.MatchLabels).AsSelector()
+
+		// List all pods in the namespace that match the label selector
+		podList, err := clientset.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{
+			LabelSelector: selector.String(),
+		})
+		Expect(err).NotTo(HaveOccurred())
+
+		for _, pod := range podList.Items {
+			// Verify the pod's start time
+			Expect(pod.Status.StartTime.Time).To(BeTemporally(">", time.Now().Add(-5*time.Minute), time.Second))
+
+			// Verify annotations related to restart information
+			Eventually(func() bool {
+				// Check if the annotation exists and is not empty
+				restartedAtExists := pod.Annotations["kubectl.kubernetes.io/restartedAt"] != ""
+				restartedBy := pod.Annotations["kubectl.kubernetes.io/restartedBy"] == "flipper-operator"
+				restartedByCRExists := pod.Annotations["flipper.example.com/restartedByCR"] != ""
+				restartedByCRDKind := pod.Annotations["flipper.example.com/restartedByCRDKind"] == "rollingupdate"
+
+				return restartedAtExists && restartedBy && restartedByCRExists && restartedByCRDKind
+			}, 2*time.Minute, 5*time.Second).Should(BeTrue())
+		}
+	}
+
+	// Helper function to verify RollingUpdate CR and deployment pods
+	verifyRollingUpdate := func(name, namespace string, interval time.Duration, expectedDeployments []string) {
+		// Define GVR for RollingUpdate
+		rollingUpdateGVR := schema.GroupVersionResource{
+			Group:    "flipper.example.com",
+			Version:  "v1alpha1",
+			Resource: "rollingupdates",
+		}
+
+		var rollingUpdate *unstructured.Unstructured
+		Eventually(func() error {
+			var err error
+			rollingUpdate, err = dynamicClient.Resource(rollingUpdateGVR).Namespace(namespace).Get(context.Background(), name, metav1.GetOptions{})
+			if err != nil {
+				return err
+			}
+
+			_, found, err := unstructured.NestedFieldCopy(rollingUpdate.Object, "status", "lastRolloutTime")
+			if err != nil {
+				return err
+			}
+			if !found {
+				return fmt.Errorf("lastRolloutTime not found")
+			}
+
+			deploymentsList, found, err := unstructured.NestedStringSlice(rollingUpdate.Object, "status", "deployments")
+			if err != nil {
+				return err
+			}
+			if len(expectedDeployments) == 0 {
+				if found {
+					return fmt.Errorf("deployments found")
+				}
+			} else {
+				if !found {
+					return fmt.Errorf("deployments not found")
+				}
+			}
+
+			// equalStringSlicesWithoutOrder checks if two slices of strings are equal (same elements, regardless of order)
+			equalStringSlicesWithoutOrder := func(a, b []string) bool {
+				if len(a) != len(b) {
+					return false
+				}
+
+				// Sort both slices
+				sortedA := make([]string, len(a))
+				sortedB := make([]string, len(b))
+				copy(sortedA, a)
+				copy(sortedB, b)
+				sort.Strings(sortedA)
+				sort.Strings(sortedB)
+
+				// Compare sorted slices
+				for i := range sortedA {
+					if sortedA[i] != sortedB[i] {
+						return false
+					}
+				}
+
+				return true
+			}
+
+			if !equalStringSlicesWithoutOrder(deploymentsList, expectedDeployments) {
+				return fmt.Errorf("deploymentsList does not match expectedDeployments")
+			}
+
+			return nil
+		}, 5*time.Minute, 5*time.Second).Should(Succeed())
+
+		// Check if the deployments' pods have restarted
+		for _, deploymentName := range expectedDeployments {
+			checkDeploymentPodsRestarted(deploymentName, namespace)
+		}
+	}
+
+	checkDeploymentPodsReady := func(deploymentNames []string, namespace string) bool {
+		for _, deploymentName := range deploymentNames {
+			deployment, err := clientset.AppsV1().Deployments(namespace).Get(context.Background(), deploymentName, metav1.GetOptions{})
+			if err != nil {
+				return false
+			}
+
+			// Construct a label selector that matches all labels in the deployment's pod template
+			selector := labels.Set(deployment.Spec.Selector.MatchLabels).AsSelector()
+
+			// List all pods in the namespace that match the label selector
+			podList, err := clientset.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{
+				LabelSelector: selector.String(),
+			})
+			if err != nil {
+				return false
+			}
+
+			// Check readiness for each pod in the deployment
+			for _, pod := range podList.Items {
+				for _, condition := range pod.Status.Conditions {
+					if condition.Type == corev1.PodReady && condition.Status != corev1.ConditionTrue {
+						return false
+					}
+				}
+			}
+		}
+
+		return true
+	}
+
+	Context("Namespace test-1", func() {
+		It("should validate RollingUpdate CR and deployment pods test-1 namespace", func() {
+			namespace := "test-1"
+			expectedDeployments := []string{"nginx-deployment-v1", "nginx-deployment-v2", "nginx-deployment-v3"}
+
+			// Apply the deployment YAML
+			By("applying the deployment YAML")
+			applyManifest("config/test/deployment-1.yaml")
+
+			// Wait for deployment pods to be running
+			By("waiting for deployment pods to be running")
+			Eventually(func() bool {
+				return checkDeploymentPodsReady(expectedDeployments, namespace)
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), "Deployment pods did not become ready in time")
+
+			// Apply the RollingUpdate CR YAML
+			By("applying the RollingUpdate CR YAML")
+			applyManifest("config/test/rollingupdate-1.yaml")
+
+			time.Sleep(30)
+			// Wait for deployment pods to be rolling restart
+			By("waiting for deployment pods to be running")
+			Eventually(func() bool {
+				return checkDeploymentPodsReady(expectedDeployments, namespace)
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), "Deployment pods did not become ready in time")
+
+			verifyRollingUpdate("rollingupdate-1", namespace, 3*time.Minute, expectedDeployments)
+		})
+	})
+
+	Context("Namespace test-2", func() {
+		It("should validate RollingUpdate CR and deployment pods test-2 namespace", func() {
+			namespace := "test-2"
+			expectedDeployments := []string{"nginx-deployment"}
+
+			// Apply the deployment YAML
+			By("applying the deployment YAML")
+			applyManifest("config/test/deployment-2.yaml")
+
+			// Wait for deployment pods to be running
+			By("waiting for deployment pods to be running")
+			Eventually(func() bool {
+				return checkDeploymentPodsReady(expectedDeployments, namespace)
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), "Deployment pods did not become ready in time")
+
+			// Apply the RollingUpdate CR YAML
+			By("applying the RollingUpdate CR YAML")
+			applyManifest("config/test/rollingupdate-2.yaml")
+
+			time.Sleep(30)
+			// Wait for deployment pods to be rolling restart
+			By("waiting for deployment pods to be running")
+			Eventually(func() bool {
+				return checkDeploymentPodsReady(expectedDeployments, namespace)
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), "Deployment pods did not become ready in time")
+
+			verifyRollingUpdate("rollingupdate-2", namespace, 5*time.Minute, expectedDeployments)
+		})
+	})
+
+	Context("Namespace test-3", func() {
+		It("should validate RollingUpdate CR and deployment pods", func() {
+			namespace := "test-3"
+			expectedDeployments := []string{}
+
+			// Apply the deployment YAML
+			By("applying the deployment YAML")
+			applyManifest("config/test/deployment-3.yaml")
+
+			// Wait for deployment pods to be running
+			By("waiting for deployment pods to be running")
+			Eventually(func() bool {
+				return checkDeploymentPodsReady(expectedDeployments, namespace)
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), "Deployment pods did not become ready in time")
+
+			// Apply the RollingUpdate CR YAML
+			By("applying the RollingUpdate CR YAML")
+			applyManifest("config/test/rollingupdate-3.yaml")
+
+			time.Sleep(30)
+			// Wait for deployment pods to be rolling restart
+			By("waiting for deployment pods to be running")
+			Eventually(func() bool {
+				return checkDeploymentPodsReady(expectedDeployments, namespace)
+			}, 5*time.Minute, 5*time.Second).Should(BeTrue(), "Deployment pods did not become ready in time")
+
+			verifyRollingUpdate("rollingupdate-3", namespace, 10*time.Minute, expectedDeployments)
 		})
 	})
 })
